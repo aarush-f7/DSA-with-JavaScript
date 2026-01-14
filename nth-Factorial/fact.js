b9731ffedafcb56c744010950aa58fff6da712f6
Aarush Rajak <rajakaarush627@gmail.com>
2026-01-14 10:11:34 +0530

    Factorial of nth number

---
+++ nth-Factorial/fact.js
@@ -0,0 +1,26 @@
+// factorial of n numbers :---------
+
+let show = prompt("Write the value here");
+
+if (show === null){
+    console.log("cancelled");
+}
+
+else{
+
+    let n = Number(show);
+
+if (isNaN(n)) {
+  console.log("invalid input");
+} else { 
+  if (n > 0) {
+    var fact = 1;
+    for (let i = 1; i <= n; i++) {
+      fact *= i;
+    }
+    console.log(fact);
+  } else {
+    console.log("write +ve value and more than 0");
+  }
+}
+}
